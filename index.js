2309ea39f8a9d06f723516f7fbaafe7f1a054445
jeel narola <narolajeel29@gmail.com>
2023-08-06 12:11:51 +0530

    Logo \ Sing-Up From

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
+// password show
+let loadicon=document.querySelector(".fa-eye")
+function myfuncton(){
+    var a=document.querySelector(".pass")
+
+    if(a.type=="password"){
+        a.type="text"
+    }
+    else{
+        a.type="password"
+    }
+    if( loadicon.classList.contains("fa-eye")){
+        loadicon.classList.replace("fa-eye","fa-eye-slash")
+    }
+    else{
+        loadicon.classList.replace("fa-eye-slash","fa-eye")
+    }
+}
+document.querySelector(".fa-eye").addEventListener("click",myfuncton)
+
+// load page
+
+
+
+window.addEventListener("load",function(){
+    setTimeout(
+        function open(event){
+            document.querySelector("#loadfrom").style.display="block";
+        },
+        2000
+    )
+})
+
+document.getElementById("loadfrom").addEventListener("submit",()=>{
+    document.getElementById("loadfrom").style.display="none"
+})
+
+
+
 // toggle icon
 
+
+
+
 let headericon=document.querySelector(".fa-bars")
+// let loadicon=document.querySelector(".fa-eye")
 const header=()=>{
     menu.classList.add("menu-show")
 }
 const headerclose=()=>{
     menu.classList.remove("menu-show")
 }
-document.querySelector(".toggle").addEventListener("click",()=>{
+document.querySelector("#toggle , .fa-eye").addEventListener("click",()=>{
     let menu=document.querySelector(".menu-name").value
     console.log(menu);
     if(headericon.classList.contains("fa-bars")){
@@ -18,13 +61,21 @@ document.querySelector(".toggle").addEventListener("click",()=>{
         headericon.classList.replace("fa-xmark","fa-bars")
         headerclose()
     }
+    // if(loadicon.classList.contains("fa-eye")){
+    //     loadicon.classList.replace("fa-eye","fa-eye-slash")
+
+    // }
+    // else{
+    //     loadicon.classList.replace("fa-eye-slash","fa-eye")
+
+    // }
 })
 
 
             // LOGIN CLICk TO LOGIN FORM OPEN AND CLOSE
 
 
-document.querySelector("#login").addEventListener("click",(e)=>{
+document.getElementById("login").addEventListener("click",(e)=>{
     e.preventDefault()
     document.querySelector("#form").style.visibility= "visible"    
 
@@ -67,10 +118,11 @@ e.preventDefault()
         document.getElementById("p2").innerHTML="Plz Enter A Password !"
     }
     else if(password.length<8){
-        document.getElementById("p2").innerHTML="Plz Enter A Password aa !"
+        document.getElementById("p2").innerHTML="Plz Enter A Password 9 Digit !"
     }
-    else{
+    if(username.length!=0&&password.length>8){
         post()
+
     }
 })
 const get=async()=>{
@@ -79,3 +131,81 @@ const get=async()=>{
     .then((data)=>console.log(data))
 }
 get()
+
+
+// SING-UP form logic
+
+
+document.getElementById("singup").addEventListener("click",()=>{
+    document.querySelector(".singup-bg").style.display="block"
+})
+document.getElementById("singup-close").addEventListener("click",()=>{
+    document.querySelector(".singup-bg").style.display="none"
+})
+
+const singuppost=async()=>{
+    let data={
+        firstname:document.getElementById("firstname").value,
+        lastname:document.getElementById("lastname").value,
+        email:document.getElementById("email").value,
+        password:document.getElementById("password").value
+    }
+    fetch("http://localhost:8090/sing-up",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(data)
+    })
+}
+document.getElementById("singup-form").addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let singupdata={
+        firstname:document.getElementById("firstname").value,
+        lastname:document.getElementById("lastname").value,
+        email:document.getElementById("email").value,
+        password:document.getElementById("password").value
+    }
+console.log(singupdata);
+        let firstname=document.getElementById("firstname").value
+        let lastname=document.getElementById("lastname").value
+        let email=document.getElementById("email").value
+        let password=document.getElementById("password").value
+        if(firstname.length==0){
+            document.querySelector(".p").innerHTML="enter a first name !"
+        }
+        else{
+            document.querySelector(".p").innerHTML=""
+
+        } 
+        if(lastname.length==0){
+            document.querySelector(".p2").innerHTML="enter a last name !"
+        }
+        else {
+            document.querySelector(".p2").innerHTML=""
+
+        }
+        if(email.length<5 || email.length==0){
+            document.querySelector(".p3").innerHTML="not email"
+        }
+        else{
+            document.querySelector(".p3").innerHTML=""
+        }
+        if(password==0){
+            document.querySelector(".p4").innerHTML="Plz Enter A Password !"
+        }
+        else{
+            document.querySelector(".p4").innerHTML=""  
+        }
+        if(firstname.lastname!=0&&lastname.length!=0&&email.length>5&&email.length!=0&&password.length1!=0){
+            singuppost()
+        }
+})
+
+const loginget=async()=>{
+    fetch("http://localhost:8090/sing-up")
+    .then((res)=>res.then)
+    .then((data)=>console.log(data))
+}
+loginget()
+
+
+
---
+++ javasvript/from.js
@@ -0,0 +1,34 @@
+// // password show
+// let loadicon=document.querySelector(".fa-eye")
+// function myfuncton(){
+//     var a=document.getElementById("password")
+
+//     if(a.type=="password"){
+//         a.type="text"
+//         // loadicon.classList.replace("fa-lock","fa-lock-open")
+//         // loadicon.classList.replace("fa-lock","fa-lock-open")
+//     }
+//     else{
+//         a.type="password"
+//     }
+//     if( loadicon.classList.contains("fa-eye")){
+//         loadicon.classList.replace("fa-eye","fa-eye-slash")
+//     }
+//     else{
+//         loadicon.classList.replace("fa-eye-slash","fa-eye")
+//     }
+// }
+// document.querySelector(".fa-eye").addEventListener("click",myfuncton)
+
+// // load page
+
+
+
+// // window.addEventListener("load",function(){
+// //     setTimeout(
+// //         function open(event){
+// //             document.querySelector("#loadfrom").style.display="block";
+// //         },
+// //         3000
+// //     )
+// // })
